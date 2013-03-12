d0d78f6dc5e827fc881a11345c8cccb02f911115
Bo Wang <david.b.wang@gmail.com>
2013-03-12 16:49:38 -0700

    Renamed test.sh to run.sh, removed nedata2.txt

--- prr/run.sh
+++ prr/run.sh
@@ -20,30 +20,22 @@ exptid=`date +%b%d-%H:%M`
 rootdir=buffersizing-$exptid
 plotpath=util
 iperf=~/iperf-patched/src/iperf
+dir=test_output_$(uname -r)
+iface=s1-eth2
 
-# TODO: change the interface name for which queue size is adjusted
-# Links are numbered as switchname-eth1,2,etc in the order they are
-# added to the topology.
-iface=s1-eth3
+mn -c
 
-for run in 1; do
-for flows_per_host in 1 2 5 10 50 100 200 300 400; do
-	dir=$rootdir/nf$flows_per_host-r$run
-
-	python buffersizing.py --bw-host 1000 \
-		--bw-net 62.5 \
-		--delay 43.5 \
+python buffersizing.py --bw-host 1000 \
+		--bw-net 10 \
+		--delay 100 \
 		--dir $dir \
-		--nflows $flows_per_host \
-		-n 3 \
-		--iperf $iperf
+		--nflows 0 \
+		--iperf $iperf \
+                --loss 0 \
+                --maxq 100 \
+                --samples 200 \
+                --index 0 \
+                --time 600
 
-	python $plotpath/plot_queue.py -f $dir/qlen_$iface.txt -o $dir/q.png
-	python $plotpath/plot_tcpprobe.py -f $dir/tcp_probe.txt -o $dir/cwnd.png --histogram
-done
-done
-
-cat $rootdir/*/result.txt | sort -n -k 1
-python plot-results.py --dir $rootdir -o $rootdir/result.png
-echo "Started at" $start
-echo "Ended at" `date`
+python $plotpath/plot_queue.py -f $dir/qlen_$iface.txt -o $dir/q.png
+python $plotpath/plot_tcpprobe.py -f $dir/tcp_probe.txt -o $dir/cwnd.png
