3e9f25521723cf1057cff905e31c1ef57c408c80
ubuntu <ubuntu@ubuntu-11.10>
2013-03-12 22:22:38 -0700

    Added both curl and iperf in run.sh

--- run.sh
+++ run.sh
@@ -31,8 +31,17 @@ python prr.py --bw-host 1000 \
               --loss 1 \
               --maxq 100 \
               --samples 3 \
-              --index -1 \
-              --time 600
+              --index -1
+
+python prr.py --bw-host 1000 \
+              --bw-net 1.2 \
+              --delay 25 \
+              --dir $dir \
+              --nflows 0 \
+              --iperf $iperf \
+              --maxq 100 \
+              --index 0 \
+              --time 30
 
 python $plotpath/plot_queue.py -f $dir/qlen_$iface.txt -o $dir/q.png
 for input_file in $dir/tcp_probe*.txt
